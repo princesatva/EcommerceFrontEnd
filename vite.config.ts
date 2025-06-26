936220f6f5c13f2a86da363e0bf9c1e7bd0215e4
prince.satva <pm@satvasolutions.com>
2025-06-26 11:24:14 +0530

    Change in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path';
 
-const API_BASE_URL = 'https://localhost:7111/api/';
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
